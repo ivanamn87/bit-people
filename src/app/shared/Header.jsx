2dbb49cb26fec604bd89b107ed4a0d53ecc823ff
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-09-04 17:28:34 +0200

    fix conflict

--- src/app/shared/Header.jsx
+++ src/app/shared/Header.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import { IoIosRefresh } from "react-icons/io";
-
-const Header = ({ viewButtonHandler, isGridView, refreshBtnHandler}) => {
-
-
 import { FaList } from "react-icons/fa";
 import { BsGrid3X2GapFill } from "react-icons/bs";
 
-const Header = ({ viewButtonHandler, isGridView }) => {
+const Header = ({ viewButtonHandler, isGridView, refreshBtnHandler }) => {
   return (
     <header className="container-fluid bg-danger py-2 fixed-top">
       <div className="container d-flex justify-content-between px-3">
         <h3 className="text-white">BitPeople</h3>
         <div>
           <button onClick={refreshBtnHandler} type="button" className="btn bg-danger text-light">
-          < IoIosRefresh />
+            <IoIosRefresh />
+          </button>
+          <button onClick={viewButtonHandler} type="button" className="btn text-danger bg-light">
+            {isGridView ? <FaList /> : <BsGrid3X2GapFill />}
           </button>
         </div>
-        <button onClick={viewButtonHandler} type="button" className="btn text-danger bg-light">
-          {isGridView ? <FaList /> : <BsGrid3X2GapFill />}
-        </button>
       </div>
     </header>
   );
