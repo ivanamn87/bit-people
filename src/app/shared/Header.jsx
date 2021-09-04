93ebc2d056bb383cb0d93370fb0b2af03f730f5c
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-09-04 16:48:46 +0200

    add grid or list view icons

--- src/app/shared/Header.jsx
+++ src/app/shared/Header.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { FaList } from "react-icons/fa";
+import { BsGrid3X2GapFill } from "react-icons/bs";
 
-const Header = ({viewButtonHandler, isGridView}) => {
+const Header = ({ viewButtonHandler, isGridView }) => {
   return (
     <header className="container-fluid bg-danger py-2 fixed-top">
       <div className="container d-flex justify-content-between px-3">
         <h3 className="text-white">BitPeople</h3>
-        <div>
-          <button onClick={viewButtonHandler} type="button" className="btn text-danger bg-light">
-            {isGridView ? "List View" : "Grid View"}
-          </button>
-        </div>
+
+        <button onClick={viewButtonHandler} type="button" className="btn text-danger bg-light">
+          {isGridView ? <FaList /> : <BsGrid3X2GapFill />}
+        </button>
       </div>
     </header>
   );
