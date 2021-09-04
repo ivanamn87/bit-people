9a8dcf379c184e6e3ab84c5577c8663593a1ee55
ninamn87 <ivanamilenovic87@gmail.com>
2021-09-04 16:52:29 +0200

    add refresh button icon

--- src/app/shared/Header.jsx
+++ src/app/shared/Header.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { IoIosRefresh } from "react-icons/io";
 
-const Header = ({viewButtonHandler, isGridView}) => {
+const Header = ({ viewButtonHandler, isGridView }) => {
+
+  function refresh() {
+    
+  
+  }
+
+
   return (
     <header className="container-fluid bg-danger py-2 fixed-top">
       <div className="container d-flex justify-content-between px-3">
         <h3 className="text-white">BitPeople</h3>
         <div>
-          <button onClick={viewButtonHandler} type="button" className="btn text-danger bg-light">
+          <button className="btn bg-danger text-light">
+            < IoIosRefresh />
+          </button>
+          <button
+            onClick={viewButtonHandler}
+            type="button"
+            className="btn text-danger bg-light"
+          >
             {isGridView ? "List View" : "Grid View"}
           </button>
         </div>
